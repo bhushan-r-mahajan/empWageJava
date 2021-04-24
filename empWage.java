c1aa896d66a7312bf3d400149c9b011ad9cfe307
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-24 11:37:59 +0530

    Added calculation of daily wage

--- empWage.java
+++ empWage.java
@@ -3,10 +3,17 @@ public class empWage {
 	public static void main (String[] args) {
 
 		int isFullTime = 1;
+		int wagePerHour = 20;
+		int workHours = 0;
 		double attendance = Math.floor(Math.random() * 10) % 2;
-		if (attendance == 1)
+		if (attendance == 1){
 			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+			workHours = 8;
+			}
+		else{
+			System.out.println("Employee is Absent");}
+
+		int dailyWage = (wagePerHour * workHours);
+		System.out.println (dailyWage);
 	}
 }
