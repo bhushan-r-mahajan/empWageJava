d2006d8288728cf14c5de83c29ad044164e4ee26
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-24 12:16:55 +0530

    Added calcuation for 20 days of work

--- empWage.java
+++ empWage.java
@@ -6,21 +6,27 @@ public class empWage {
 		int isPartTime = 2;
 		int wagePerHour = 20;
 		int workHours = 0;
-		double attendance = Math.floor(Math.random() * 10) % 3;
-		switch ((int) attendance ) {
-			case 1:
-				System.out.println("Employee is Full Time");
-				workHours = 8;
-				break;
-			case 2:
-				System.out.println("Employee is Part Time");
-				workHours = 4;
-				break;
-			default:
-			System.out.println("Employee is Absent");
-		}
 
-		int dailyWage = (wagePerHour * workHours);
-		System.out.println (dailyWage);
+		for (int i = 0; i<20; i++)
+		{
+			double attendance = Math.floor(Math.random() * 10) % 3;
+			switch ((int) attendance )
+			{
+				case 1:
+					System.out.println("Employee is FullTime");
+					workHours = 8;
+					break;
+				case 2:
+					System.out.println("Employee is PartTime");
+					workHours = 4;
+					break;
+				default:
+					System.out.println("Employee is Absent");
+					workHours = 0;
+					break;
+			}
+			int dailyWage = (wagePerHour * workHours);
+			System.out.println (dailyWage);
+		}
 	}
 }
