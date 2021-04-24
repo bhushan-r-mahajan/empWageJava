56b5a8d90f2f1f84e67ccdeca4d943fac672a83c
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-24 12:03:13 +0530

    Added switch case instead of if else

--- empWage.java
+++ empWage.java
@@ -3,17 +3,22 @@ public class empWage {
 	public static void main (String[] args) {
 
 		int isFullTime = 1;
+		int isPartTime = 2;
 		int wagePerHour = 20;
 		int workHours = 0;
 		double attendance = Math.floor(Math.random() * 10) % 3;
-		if (attendance == 1){
-			System.out.println("Employee is Full Time");
-			workHours = 8;}
-		else if (attendance == 2){
-			System.out.println("Employee is Part Time");
-			workHours = 4;}
-		else{
-			System.out.println("Employee is Absent");}
+		switch ((int) attendance ) {
+			case 1:
+				System.out.println("Employee is Full Time");
+				workHours = 8;
+				break;
+			case 2:
+				System.out.println("Employee is Part Time");
+				workHours = 4;
+				break;
+			default:
+			System.out.println("Employee is Absent");
+		}
 
 		int dailyWage = (wagePerHour * workHours);
 		System.out.println (dailyWage);
